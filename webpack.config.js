c239d9a4655863a2f4d01d46f8b1acfe60a67b86
Strada Fabio <fabio.strada@allitude.it>
2022-02-21 17:11:13 +0100

    fix shared singleton service

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ const share = mf.share;
 
 const sharedMappings = new mf.SharedMappings();
 sharedMappings.register(
-  path.join(__dirname, 'tsconfig.json'),
-  ["my-authenticator-lib"]);
+  path.join(__dirname, 'tsconfig.json'));
 
 module.exports = {
   output: {
@@ -42,11 +41,11 @@ module.exports = {
         // },
 
         shared: share({
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-          "my-authenticator-lib": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto', eager: true }, 
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto', eager: true }, 
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto', eager: true }, 
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto', eager: true },
+          "my-authenticator-lib": { singleton: true, strictVersion: true, requiredVersion: 'auto', eager: true },
           ...sharedMappings.getDescriptors()
         })
         
